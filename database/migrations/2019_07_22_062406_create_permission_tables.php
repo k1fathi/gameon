59b09a1ee7e99604959c0ea0853109fecbd18986
Jawad Fathi <javad.fathi@sarente.com>
2019-07-23 10:02:49 +0300

    Role manager works fine

--- database/migrations/2019_07_22_062406_create_permission_tables.php
+++ database/migrations/2019_07_22_062406_create_permission_tables.php
@@ -26,7 +26,7 @@ class CreatePermissionTables extends Migration
         Schema::create($tableNames['roles'], function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('guard_name');
+            $table->string('guard_name')->nullable();
             $table->timestamps();
         });
 
