ad474674b8b2abb603878831354dbee2eaf3449d
yusufakyol <yusufakyol2597@gmail.com>
2019-07-23 16:57:56 +0300

    Controller address repaired

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Api\Controller;
+use App\Http\Controllers\Controller;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
