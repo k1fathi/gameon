e81a527da73489064c7e0265b38c42d9fc345681
Jawad Fathi <javad.fathi@sarente.com>
2019-07-24 16:48:14 +0300

    Exception handler model not found added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Tymon\JWTAuth\Exceptions;
 
@@ -11,7 +12,6 @@ class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
-     *
      * @var array
      */
     protected $dontReport = [
@@ -20,7 +20,6 @@ class Handler extends ExceptionHandler
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
-     *
      * @var array
      */
     protected $dontFlash = [
@@ -30,8 +29,7 @@ class Handler extends ExceptionHandler
 
     /**
      * Report or log an exception.
-     *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
      * @return void
      */
     public function report(Exception $exception)
@@ -41,24 +39,26 @@ class Handler extends ExceptionHandler
 
     /**
      * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $exception
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
+
         if ($exception instanceof Exceptions\TokenExpiredException) {
             return response()->error('auth.token_expired', $exception->getStatusCode(), [], 401);
         } else if ($exception instanceof Exceptions\TokenInvalidException) {
             return response()->error('auth.token_invalid', $exception->getStatusCode(), [], 401);
+        } else if ($exception instanceof ModelNotFoundException) {
+            return response()->error('common.not-found', $exception->getStatusCode(), [], 404);
         }
 
         return parent::render($request, $exception);
     }
+
     /**
      * Convert an authentication exception into an unauthenticated response.
-     *
      * @param  \Illuminate\Http\Request $request
      * @param  \Illuminate\Auth\AuthenticationException $exception
      * @return \Illuminate\Http\Response
