8c7692464a0215d61bfd1536ad87ce1ea3b60438
Jawad Fathi <javad.fathi@sarente.com>
2019-08-07 11:27:42 +0300

    Dockerfile added.

--- app/Models/Accessory.php
+++ app/Models/Accessory.php
@@ -5,6 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
 
+/**
+ * App\Models\Accessory
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Activitylog\Models\Activity[] $activities
+ * @property-read \App\Models\Image $image
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Project[] $projects
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Accessory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Accessory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Accessory query()
+ * @mixin \Eloquent
+ */
 class Accessory extends Model
 {
     //
--- app/Models/ClassroomTranslation.php
+++ app/Models/ClassroomTranslation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\ClassroomTranslation
  *
- * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Activitylog\Models\Activity[] $activities
+ * @property-read \App\Models\Classroom $classroom
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ClassroomTranslation newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ClassroomTranslation newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ClassroomTranslation query()
