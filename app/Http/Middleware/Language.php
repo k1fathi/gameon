2ab2747970355469531970598a5b8c167ee902be
Jawad Fathi <javad.fathi@sarente.com>
2019-08-11 01:19:51 +0300

    Add language db to platform

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -17,7 +17,7 @@ class Language
      */
     public function handle($request, Closure $next)
     {
-        // TODO: "tr-TR,tr;q=0.8,en-US;q=0.6,en;q=0.4" gibi priority için iyileştirme
+        // TODO: "tr-TR,tr;q=0.8,en-US;q=0.6,en;q=0.4"
         $language = $request->header('Accept-Language');
 
         view()->share('locales', \App\Models\Language::locales());
