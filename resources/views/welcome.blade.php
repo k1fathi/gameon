7947ac377ca123d5d64e2f5655a3c13cb1a34090
Jawad Fathi <javad.fathi@sarente.com>
2019-08-14 14:08:15 +0300

    welcome update 4 test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>GameOn!!! Platform</title>
+        <title>GameOn Platform</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    GameOn Platform
+                    GameOn! Platform
                 </div>
             </div>
         </div>
